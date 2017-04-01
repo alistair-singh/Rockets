91242397c528c6d78e229e3c6abc0aaf65057b7c
Alistair Singh <alistair.singh7@gmail.com>
2017-04-01 08:52:40 +0200

    set up the basic scene

--- src/Rockets/main.cpp
+++ src/Rockets/main.cpp
@@ -12,15 +12,16 @@ class RocketsApp : public App {
 public:
   void setup() override {
     // Set up the camera.
-    mCamera.lookAt(vec3(2.0f, 3.0f, 1.0f), vec3(0));
-    mCamera.setPerspective(40.0f, getWindowAspectRatio(), 0.01f, 100.0f);
+    mCamera.lookAt(vec3(30, 44, 30), vec3(0,20.0f,0));
+    mCamera.setPerspective(40.0f, getWindowAspectRatio(), 0.01f, 1000.0f);
     mCamUi = CameraUi(&mCamera, getWindow());
 
-    // Create batches that render fast.
-    auto lambertShader = gl::getStockShader(gl::ShaderDef().color().lambert());
-    auto colorShader = gl::getStockShader(gl::ShaderDef().color());
+    // Create batches that render fast.x
+    auto colorShader = gl::getStockShader(gl::ShaderDef().color().lambert());
 
-    mFloor = gl::Batch::create(geom::WirePlane().size(vec2(1000)).subdivisions(ivec2(1000)), colorShader);
+    mFloor = gl::Batch::create(geom::Plane().size(vec2(30)).subdivisions(ivec2(30)), colorShader);
+    mRef = gl::Batch::create(geom::Sphere(), colorShader);
+    mCenter = gl::Batch::create(geom::Sphere(), colorShader);
   }
 
   void update() override {
@@ -28,7 +29,7 @@ public:
 
   void draw() override {
     // Gray background.
-    gl::clear(Color::gray(0.5f));
+    gl::clear(Color::hex(0x251838));
 
     // Set up the camera.
     gl::ScopedMatrices push;
@@ -37,15 +38,42 @@ public:
     // Enable depth buffer.
     gl::ScopedDepth depth(true);
 
-    // Draw the grid on the floor.
+    // Draw the centre.
     {
-      gl::ScopedColor color(Color::gray(0.2f));
+      gl::ScopedColor color(Color::hex(0x5492f7));
+      gl::ScopedModelMatrix model;
+
+      gl::multModelMatrix(glm::scale(vec3(0.5f)));
+      mCenter->draw();
+    }
+
+    // Draw the end.
+    {
+      gl::ScopedColor color(Color::hex(0x5492f7));
+      gl::ScopedModelMatrix model;
+
+      gl::multModelMatrix(glm::translate(vec3(0, 35.0f, 0)) * glm::scale(vec3(0.5f)));
+      mCenter->draw();
+    }
+
+    // Draw the ref.
+    {
+      gl::ScopedColor color(Color::hex(0xf44242));
+      gl::ScopedModelMatrix model;
+
+      gl::multModelMatrix(glm::translate(vec3(1,2,3)) * glm::scale(vec3(0.5f)));
+      mRef->draw();
+    }
+
+    // Draw the floor.
+    {
+      gl::ScopedColor color(Color::hex(0x183818));
       mFloor->draw();
     }
   }
 
 private:
-  gl::BatchRef mFloor;
+  gl::BatchRef mFloor, mRef, mCenter;
 
   CameraPersp mCamera;
   CameraUi mCamUi;
